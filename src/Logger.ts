33d901be1674c3be08cb76bd2c02585911c85120
smycynek <sv@stevenvictor.net>
2025-11-27 07:40:59 -0500

    log level improvement

--- src/Logger.ts
+++ src/Logger.ts
@@ -2,20 +2,38 @@
 
 import * as tf from '@tensorflow/tfjs';
 
+// I didn't feel like installing a logging package, so this is fine for now.
+
+export enum LoggerLevel {
+  None,
+  Info,
+  Trace,
+}
+
 export class Logger {
   private constructor() {}
-  public static enabled: boolean = false;
+  public static loggerLevel: LoggerLevel = LoggerLevel.Info;
   static info(prefix: string, data: string | tf.Tensor | tf.Tensor2D) {
-    if (Logger.enabled) {
-      console.log(prefix);
-      if (data instanceof tf.tensor2d) {
-        data.print();
-      } else if (data instanceof tf.Tensor) {
-        data.print();
-      } else {
-        console.log(data);
-      }
-      console.log('---');
+    if (Logger.loggerLevel !== LoggerLevel.None) {
+      Logger.implementation(`INFO: ${prefix}`, data);
     }
+  }
+
+  static trace(prefix: string, data: string | tf.Tensor | tf.Tensor2D) {
+    if (Logger.loggerLevel === LoggerLevel.Trace) {
+      Logger.implementation(`TRACE: ${prefix}`, data);
+    }
+  }
+
+  static implementation(prefix: string, data: string | tf.Tensor | tf.Tensor2D) {
+    console.log(prefix);
+    if (data instanceof tf.tensor2d) {
+      data.print();
+    } else if (data instanceof tf.Tensor) {
+      data.print();
+    } else {
+      console.log(data);
+    }
+    console.log('---');
   }
 }
