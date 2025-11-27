5d0f7d47a3dd6a7d889341728fea50b733ca5e0d
Steven Victor Mycynek <smycynek@users.noreply.github.com>
2025-11-27 14:45:03 -0500

    Add comments (#3)

--- src/Point.ts
+++ src/Point.ts
@@ -2,6 +2,10 @@ import { round2 } from './utility';
 
 const formatter = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2 });
 
+/*
+This class is simple enough that we barely need it, and we could enhance it further,
+but it's mostly just to conveniently accept, display, and extract point data.
+*/
 export class Point {
   public constructor(
     public x: number,
--- src/color.ts
+++ src/color.ts
@@ -3,5 +3,5 @@ export enum Color {
   red = 'rgb(255,0,0)',
   blue = 'rgb(0,0,255)',
   white = 'rgb(255,255,255)',
-  grey = 'rgba(211,211,211,0.7)',
+  grey = 'rgba(211,211,211,0.7)', // Used for translucent centroid background
 }
